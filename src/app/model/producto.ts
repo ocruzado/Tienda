54f45ad1241ef0f75ba255d13875a1f15f52fc02
ocruzado <ocruzado89@gmail.com>
2016-11-07 23:10:46 -0500

    comit

--- src/app/model/producto.ts
+++ src/app/model/producto.ts
@@ -4,12 +4,12 @@ export class Producto {
     prod_Descripcion: string;
     prod_Precio: number;
 
-    prod_FlagActivo: boolean;
-    prod_FlagEliminado: boolean;
-    prod_CreadoPor: string;
-    prod_FechaCreacion: string;
-    prod_ModificadoPor: string;
-    prod_FechaModificacion: string;
+    // prod_FlagActivo: boolean;
+    // prod_FlagEliminado: boolean;
+    // prod_CreadoPor: string;
+    // prod_FechaCreacion: string;
+    // prod_ModificadoPor: string;
+    // prod_FechaModificacion: string;
 
     constructor() {
         this.prod_Nombre = "";

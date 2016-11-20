8af1f655959a9ed63c867e9f3cbb31b117f85b91
ocruzado <ocruzado89@gmail.com>
2016-11-19 23:23:08 -0500

    comit

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,15 +1,12 @@
 import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
-//import {ProductoComponent} from './producto/producto.component';
 import {DashBoardComponent} from "./dashboard.component";
-//import {ProductoFormComponent} from "./producto/productoForm.component";
 import {ProductoComponent} from "./producto.component";
 import {CategoriaComponent} from "./categoria.component";
 
 const appRoutes: Routes = [
     {path: '', component: DashBoardComponent},
-    //{path: 'producto', component: ProductoFormComponent}
     {path: 'producto', component: ProductoComponent},
     {path: 'categoria', component: CategoriaComponent}
 ];
